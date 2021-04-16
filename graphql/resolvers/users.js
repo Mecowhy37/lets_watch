f5f8b334c9da8306d919a9450f8125fa53097bcb
Miko <kozak.mikolaj@gmail.com>
2021-04-16 19:37:55 +0200

    ...

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -4,7 +4,7 @@ module.exports = {
   Query: {
     async getUsers() {
       try {
-        const Users = await User.find();
+        const Users = await User.find().sort({ createdAt: -1 });
         return Users;
       } catch (err) {
         throw new Error(err);
